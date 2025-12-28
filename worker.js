b26f9728be808e2af394adfb92667fd03fa11a3c
Athanasios Emmanouilidis <a@emman.dev>
2025-12-29 00:10:09 +0200

    Changes
    Loading State: The "Generate Keys" button now changes to "Generating..." to indicate activity.
    
    Worker Cleanup: Removed the unnecessary async wrapper in worker.js (importScripts is synchronous).

--- worker.js
+++ worker.js
@@ -1,25 +1,21 @@
-(async () => {
+importScripts('https://cdn.jsdelivr.net/npm/openpgp@6.3.0/dist/openpgp.min.js');
 
-    importScripts('https://cdn.jsdelivr.net/npm/openpgp@6.3.0/dist/openpgp.min.js');
+self.onmessage = async function handleMessageFromMain(msg) {
 
-    self.onmessage = async function handleMessageFromMain(msg) {
+    try {
+        const { privateKey, publicKey, revocationCertificate } = await openpgp.generateKey({
+            type: msg.data.optionsTypeValue, // Type of the key, defaults to ECC
+            curve: 'curve25519', // ECC curve name, defaults to curve25519
+            rsaBits: parseInt(msg.data.optionsKeySizeValue),
+            userIDs: [{ name: msg.data.optionsNameValue, email: msg.data.optionsEmailValue }], // you can pass multiple user IDs
+            passphrase: msg.data.optionsPassphraseValue, // protects the private key
+            keyExpirationTime: parseInt(msg.data.optionsExpirationTimeValue),
+            format: 'armored' // output key format, defaults to 'armored' (other options: 'binary' or 'object')
+        });
 
-        try {
-            const { privateKey, publicKey, revocationCertificate } = await openpgp.generateKey({
-                type: msg.data.optionsTypeValue, // Type of the key, defaults to ECC
-                curve: 'curve25519', // ECC curve name, defaults to curve25519
-                rsaBits: parseInt(msg.data.optionsKeySizeValue),
-                userIDs: [{ name: msg.data.optionsNameValue, email: msg.data.optionsEmailValue }], // you can pass multiple user IDs
-                passphrase: msg.data.optionsPassphraseValue, // protects the private key
-                keyExpirationTime: parseInt(msg.data.optionsExpirationTimeValue),
-                format: 'armored' // output key format, defaults to 'armored' (other options: 'binary' or 'object')
-            });
+        postMessage({ privateKey, publicKey, revocationCertificate });
+    } catch (error) {
+        postMessage({ error: error.message });
+    }
 
-            postMessage({ privateKey, publicKey, revocationCertificate });
-        } catch (error) {
-            postMessage({ error: error.message });
-        }
-
-    };
-
-})();
+};
