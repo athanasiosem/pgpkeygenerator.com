aba564f45259aa3df2240f4d340c07b9e44e9986
Athanasios Emmanouilidis <a@emman.dev>
2025-12-28 23:24:39 +0200

    Put inline CSS code to a file. Updated worker.js exception handling.

--- worker.js
+++ worker.js
@@ -4,17 +4,21 @@
 
     self.onmessage = async function handleMessageFromMain(msg) {
 
-        const { privateKey, publicKey, revocationCertificate } = await openpgp.generateKey({
-            type: msg.data.optionsTypeValue, // Type of the key, defaults to ECC
-            curve: 'curve25519', // ECC curve name, defaults to curve25519
-            rsaBits: msg.data.optionsKeySizeValue,
-            userIDs: [{ name: msg.data.optionsNameValue, email: msg.data.optionsEmailValue }], // you can pass multiple user IDs
-            passphrase: msg.data.optionsPassphraseValue, // protects the private key
-            keyExpirationTime: msg.data.optionsExpirationTimeValue,
-            format: 'armored' // output key format, defaults to 'armored' (other options: 'binary' or 'object')
-        });
+        try {
+            const { privateKey, publicKey, revocationCertificate } = await openpgp.generateKey({
+                type: msg.data.optionsTypeValue, // Type of the key, defaults to ECC
+                curve: 'curve25519', // ECC curve name, defaults to curve25519
+                rsaBits: msg.data.optionsKeySizeValue,
+                userIDs: [{ name: msg.data.optionsNameValue, email: msg.data.optionsEmailValue }], // you can pass multiple user IDs
+                passphrase: msg.data.optionsPassphraseValue, // protects the private key
+                keyExpirationTime: msg.data.optionsExpirationTimeValue,
+                format: 'armored' // output key format, defaults to 'armored' (other options: 'binary' or 'object')
+            });
 
-        postMessage({ privateKey, publicKey, revocationCertificate });
+            postMessage({ privateKey, publicKey, revocationCertificate });
+        } catch (error) {
+            postMessage({ error: error.message });
+        }
 
     };
 
